fecd31c14f7c52c0bef2fbe79a0b6dd7bc784450
Ruslan Dolibskyi <dolibskiyruslan.gmail.com>
2024-05-13 18:44:08 +0300

    HW-7

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const categories = document.querySelector('#categories');
+
+const items = categories.querySelectorAll('.item');
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach(item => {
+  const header = item.querySelector('h2').textContent;
+
+  const listItems = item.querySelectorAll('ul li');
+
+  console.log(`Category: ${header}`);
+  console.log(`Elements: ${listItems.length}`);
+});
